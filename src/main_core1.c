8c980b44dee5922a8eb86aab32eca8692488b1c7
yire <yire@kth.se>
2026-04-02 20:11:27 +0200

    feat: add one more task

--- src/main_core1.c
+++ src/main_core1.c
@@ -7,11 +7,13 @@
 #include <test_env.h>
 #include "PredictOS.h"
 
-PDOS_DTCM uint32_t task1Stack[32];
-PDOS_DTCM uint32_t task2Stack[32];
+PDOS_DTCM uint32_t task1Stack[128];
+PDOS_DTCM uint32_t task2Stack[128];
+PDOS_DTCM uint32_t task3Stack[128];
 
 PdOSTaskHandle task1Handle;
 PdOSTaskHandle task2Handle;
+PdOSTaskHandle task3Handle;
 
 PDOS_ITCM void task1_func(void)
 {
@@ -60,7 +62,7 @@ PDOS_ITCM void task2_func(void)
 	uint8_t prevMagicNum = 12;
 
 	localMem = PdOS_read(0);
-	for (i = 0; i < 200U; i++)
+	for (i = 0; i < 100U; i++)
 	{
 		localMem[i] = prevMagicNum;
 	}
@@ -71,7 +73,7 @@ PDOS_ITCM void task2_func(void)
 		localMem = PdOS_read(5);
 
 		magicNum = prevMagicNum + 2;
-		for (i = 0; i < 200U; i++)
+		for (i = 0; i < 100U; i++)
 		{
 			if (localMem[i] != prevMagicNum)
 			{
@@ -81,11 +83,49 @@ PDOS_ITCM void task2_func(void)
 			localMem[i] = magicNum;
 		}
 		prevMagicNum = magicNum;
-		PdOS_busy_wait(45);
+		PdOS_busy_wait(25);
 
-		PdOS_write(10);
+		PdOS_write(3);
 
-		PdOS_delayUntil(&prevWkUpTime, 160);
+		PdOS_delayUntil(&prevWkUpTime, 130);
+	}
+}
+
+PDOS_ITCM void task3_func(void)
+{
+	uint32_t i;
+	uint32_t prevWkUpTime = PdOS_get_systime();
+	uint8_t *localMem;
+	uint8_t magicNum;
+	uint8_t prevMagicNum = 25;
+
+	localMem = PdOS_read(0);
+	for (i = 0; i < 100U; i++)
+	{
+		localMem[i] = prevMagicNum;
+	}
+	PdOS_write(0);
+
+	while (1)
+	{
+		localMem = PdOS_read(3);
+
+		magicNum = prevMagicNum + 2;
+		for (i = 0; i < 100U; i++)
+		{
+			if (localMem[i] != prevMagicNum)
+			{
+				// data unexpected
+				while (1);
+			}
+			localMem[i] = magicNum;
+		}
+		prevMagicNum = magicNum;
+		PdOS_busy_wait(30);
+
+		PdOS_write(3);
+
+		PdOS_delayUntil(&prevWkUpTime, 250);
 	}
 }
 
@@ -105,7 +145,8 @@ int main(void)
 	PdOS_init();
 
 	task1Handle = PdOS_create_task(&task1_func, 5U, task1Stack, sizeof(task1Stack), 100U);
-	task2Handle = PdOS_create_task(&task2_func, 2U, task2Stack, sizeof(task2Stack), 200U);
+	task2Handle = PdOS_create_task(&task2_func, 3U, task2Stack, sizeof(task2Stack), 100U);
+	task3Handle = PdOS_create_task(&task3_func, 2U, task3Stack, sizeof(task3Stack), 100U);
 
 	PdOS_run();
 }
